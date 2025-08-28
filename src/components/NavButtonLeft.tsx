7e4429f9ea3a6f9790fe393ff0702a4267c63ff5
Toby Chen <toby.chen1337@outlook.com>
2025-08-28 19:04:32 +0100

    fix nav bar opacity issues

--- src/components/NavButtonLeft.tsx
+++ src/components/NavButtonLeft.tsx
@@ -15,12 +15,7 @@ const NavButtonLeft: React.FC<NavButtonProps> = ({
   return (
     <Button
       onClick={onClick}
-      className={`fixed z-auto flex items-center gap-2 p-6 rounded-full text-primary shadow-md hover:text-background border-0 bg-background hover:shadow-lg
-        active:scale-90
-        transition-all duration-300 ease-in-out
-        lg:relative
-        bottom-4 left-4 lg:bottom-auto lg:left-auto
-        ${isOpen ? '-translate-x-200' : 'translate-x-0'} hover:-rotate-3`}
+      className={`fixed z-auto flex items-center gap-2 p-6 rounded-full text-primary shadow-md hover:text-background border-0 bg-background hover:shadow-lg transition-all duration-300 ease-in-out lg:relative bottom-4 left-4 lg:bottom-auto lg:left-auto hover:bg-primary ${isOpen ? '-translate-x-200' : 'translate-x-0'} hover:-rotate-3`}
     >
       <Hamburger toggled={isOpen} size={18} color="currentColor" />
       {!isOpen ? <span className="font-semibold tracking text-lg mr-2">{label}</span> : null}
--- src/components/NavButtonRight.tsx
+++ src/components/NavButtonRight.tsx
@@ -15,13 +15,7 @@ const NavButtonRight: React.FC<NavButtonProps> = ({
   return (
     <Button
       onClick={onClick}
-      className={`flex items-center gap-2 p-6 rounded-full text-primary shadow-md hover:text-background border-0 bg-background hover:shadow-lg
-        active:scale-90
-        transition-all duration-300 ease-in-out
-        ${isOpen ? 'translate-x-200' : 'translate-x-0'}
-        fixed lg:relative
-        bottom-4 right-4 lg:bottom-auto lg:right-auto
-        z-50 hover:rotate-3`}
+      className={`flex items-center gap-2 p-6 rounded-full text-primary shadow-md hover:text-background border-0 bg-background hover:bg-primary hover:shadow-lg active:scale-90 transition-all duration-300 ease-in-out ${isOpen ? 'translate-x-200' : 'translate-x-0'} fixed lg:relative bottom-4 right-4 lg:bottom-auto lg:right-auto z-50 hover:rotate-3`}
     >
       {!isOpen ? <span className="font-semibold tracking text-lg mr-2">{label}</span> : null}
       {!isOpen ? <Hamburger toggled={isOpen} size={18} color="currentColor" /> : <Hamburger
