59f0525d7ccd02f7016855d72463d39f269224b9
toby <toby.chen1337@outlook.com>
2025-09-07 21:35:46 +0100

    remove speed insights for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import QuestsPage from './pages/QuestsPage';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
 
@@ -10,7 +9,6 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/quests" element={<QuestsPage />} />
-      <SpeedInsights />
     </Routes>
   )
 }
