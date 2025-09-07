0f1528f0c68b70dc45a35f50c736ca781f6e0beb
toby <toby.chen1337@outlook.com>
2025-09-07 21:22:17 +0100

    add vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import QuestsPage from './pages/QuestsPage';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
 
   return (
     <Routes>
+      <SpeedInsights/>
       <Route path="/" element={<HomePage />} />
       <Route path="/quests" element={<QuestsPage />} />
     </Routes>
