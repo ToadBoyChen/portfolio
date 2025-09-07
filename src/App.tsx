4cac66f6f6c9f1b1e47e09c468be1f9f9a880cb1
toby <toby.chen1337@outlook.com>
2025-09-07 21:31:49 +0100

    change location of speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import QuestsPage from './pages/QuestsPage';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
 
   return (
     <Routes>
-      <SpeedInsights/>
       <Route path="/" element={<HomePage />} />
       <Route path="/quests" element={<QuestsPage />} />
+      <SpeedInsights />
     </Routes>
   )
 }
--- src/pages/QuestsPage.tsx
+++ src/pages/QuestsPage.tsx
@@ -7,15 +7,13 @@ const QuestsPage = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // This timer simulates loading. The loader animation itself is 6 seconds.
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 6500);
+    }, 4000);
 
     return () => clearTimeout(timer);
   }, []);
 
-  // Show the loader while isLoading is true, then show the universe.
   return (
     <>
       {isLoading ? <QuestUniverseLoader /> : <QuestUniverse />}
