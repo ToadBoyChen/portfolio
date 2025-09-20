ad643edea352d7a1299ac44f306f636bebe125dd
toby <toby.chen1337@outlook.com>
2025-09-20 15:13:01 +0100

    apply vercel speed insights and analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
