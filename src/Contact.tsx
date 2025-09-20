465a5af65db758bef9acfbd21f1cd71517dca172
toby <toby.chen1337@outlook.com>
2025-09-20 14:02:33 +0100

    fix contact img and improve grammar

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,6 +1,7 @@
 import AnimatedText from "./animation/AnimatedText";
 import Form from "./components/Form";
 import SectionCard from "./components/SectionCard";
+import mePhoto from "./assets/me/me.jpeg";
 
 function Contact() {
   return (
@@ -12,13 +13,13 @@ function Contact() {
       <div className="mt-8 w-full max-w-5xl">
         <div className="flex flex-col lg:flex-row items-center justify-center gap-6 lg:gap-10">
           <img
-            src="src/assets/me/me.jpeg"
+            src={mePhoto}
             alt="A photo of Toby Chen"
             className="w-40 h-40 sm:w-52 sm:h-52 object-cover p-2 rounded-full bg-gradient-to-r from-rose-300 via-violet-300 to-purple-300 gradient-border"
           />
           <SectionCard>
             <p className="text-center lg:text-left text-lg">
-              I'm always open to discuss and work on new things. If you like what you see, why not drop me a message below? BTW - this website is still a work in progress. If you spot anything fishy - or off, please let me know :)
+              I'm always open to discuss and work on new things. If you like what you see, why not drop me a message below? BTW - this website is still a work in progress. If you spot anything fishy or off please let me know :)
             </p>
           </SectionCard>
         </div>
