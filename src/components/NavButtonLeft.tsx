0f3962fd55528912e7dc02373b981b4c3b5f71d4
toby <toby.chen1337@outlook.com>
2025-09-20 14:28:54 +0100

    tidy up nav buttons

--- src/components/NavButtonLeft.tsx
+++ src/components/NavButtonLeft.tsx
@@ -17,7 +17,13 @@ const NavButtonLeft: React.FC<NavButtonProps> = ({
       onClick={onClick}
       className={`fixed z-auto flex items-center gap-2 p-4 sm:p-5 md:p-6 rounded-full text-primary shadow-md hover:text-background border-0 bg-background hover:shadow-lg transition-all duration-300 ease-in-out lg:relative bottom-4 left-4 lg:bottom-auto lg:left-auto hover:bg-primary ${isOpen ? '-translate-x-200' : 'translate-x-0'} hover:-rotate-3`}
     >
-      <Hamburger toggled={isOpen} size={20} color="currentColor" />
+      <div className='hidden sm:block'>
+        <Hamburger 
+          toggled={isOpen} 
+          size={20} 
+          color="currentColor" 
+        />
+      </div>
       {!isOpen ? (
         <span className="font-semibold tracking text-base sm:text-lg md:text-xl mr-2">
           {label}
--- src/components/NavButtonRight.tsx
+++ src/components/NavButtonRight.tsx
@@ -17,16 +17,18 @@ const NavButtonRight: React.FC<NavButtonProps> = ({
       onClick={onClick}
       className={`flex items-center gap-2 p-4 sm:p-5 md:p-6 rounded-full text-primary shadow-md hover:text-background border-0 bg-background hover:bg-primary hover:shadow-lg active:scale-90 transition-all duration-300 ease-in-out ${isOpen ? 'translate-x-200' : 'translate-x-0'} fixed lg:relative bottom-4 right-4 lg:bottom-auto lg:right-auto z-50 hover:rotate-3`}
     >
+      <div className='hidden sm:block'>
+        <Hamburger 
+          toggled={isOpen} 
+          size={20} 
+          color="currentColor" 
+        />
+      </div>
       {!isOpen ? (
         <span className="font-semibold tracking text-base sm:text-lg md:text-xl mr-2">
           {label}
         </span>
       ) : null}
-      {!isOpen ? (
-        <Hamburger toggled={isOpen} size={20} color="currentColor" />
-      ) : (
-        <Hamburger toggled={isOpen} size={20} color="currentColor" />
-      )}
     </Button>
   );
 };
